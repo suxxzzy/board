833df5598b10dd6fec031326b87c055cfbfd19c6
suxxzzy <abcc0785@gmail.com>
2022-06-27 09:55:27 +0900

    [add] minor changes

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -38,7 +38,7 @@ function Signup() {
     //아이디 유효성 검사 + 중복 검사
     const handleIDCheck = async (id) => {
         if (!isValidID(id)) {
-            setIDMessage('형식에 맞지 않는 아이디입니다');
+            setIDMessage('영문 숫자 조합해 4-20자로 만들어주세요');
             return false;
         }
 
